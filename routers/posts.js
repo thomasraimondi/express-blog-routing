18b0d7ce529d42ce1ac62a939852821446ce9215
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-13 13:59:20 +0200

    add route show

--- routers/posts.js
+++ routers/posts.js
@@ -9,4 +9,19 @@ router.get("/", (req, res) => {
   res.status(200).send({ status: 200, success: "ok", data: posts });
 });
 
+// # SHOW
+router.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log("richiesta per il post: " + id);
+
+  const post = posts.find((post) => post.id === id);
+
+  res.header({ "Access-Control-Allow-Origin": "*" });
+  post
+    ? res.status(200).send({ status: 200, success: "ok", data: post })
+    : res
+        .status(404)
+        .send({ status: 404, success: "ko", data: "post not found" });
+});
+
 module.exports = router;
