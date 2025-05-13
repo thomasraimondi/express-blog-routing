0994c082a206c64d8599aa32b83a31de1b18c9bc
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-13 13:55:05 +0200

    add route index

--- app.js
+++ app.js
@@ -9,24 +9,7 @@ const routerPost = require("./routers/posts");
 // * static Asset
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  console.log("richiesta ricevuta route: Home");
-  const responseData = "Server del mio blog";
-  res.header({ "Access-Control-Allow-Origin": "*" });
-  res.status(200).send(responseData);
-});
-
-app.get("/bacheca", (req, res) => {
-  console.log("richiesta ricevuta route: Bacheca");
-
-  if (posts.length > 0) {
-    res.header({ "Access-Control-Allow-Origin": "*" });
-    res.status(200).json({ posts: posts });
-  } else {
-    res.header({ "Access-Control-Allow-Origin": "*" });
-    res.status(204).json(posts);
-  }
-});
+app.use("/posts", routerPost);
 
 app.listen(appPort, () => {
   console.log(`Server in ascolto: ${appUrl}`);
--- routers/posts.js
+++ routers/posts.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { posts } = require("../data/db");
 
-// * Definizione dei router
+// # INDEX
+router.get("/", (req, res) => {
+  console.log("richiesta ricevuta route: Home");
+  res.header({ "Access-Control-Allow-Origin": "*" });
+  res.status(200).send({ status: 200, success: "ok", data: posts });
+});
 
 module.exports = router;
